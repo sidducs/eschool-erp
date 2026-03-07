c3fb7b05809c245b3a7a2b531d7bb0de18f54565
sidducs <sidducs2233@gmail.com>
2026-03-08 00:19:58 +0530

    fix: frontend api timeout

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://localhost:5000",
+  timeout: 30000, // 30 seconds
 });
 
 api.interceptors.request.use((config) => {
